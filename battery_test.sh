48ab0907d0a0b29ed2d33a69fb8b075baf401798
Vinay <vinay@Marvin.(none)>
2012-11-04 02:42:12 +0530

    updated Readme and modified battery script slightly

--- battery_test.sh
+++ battery_test.sh
@@ -2,13 +2,13 @@
 battery_charge=`acpi -b | cut -f 4 -d " " | awk 'sub("..$","")'`
 current_status=`acpi -b | cut -f 3 -d " "`
 current_status=${current_status%?}
-if [ $battery_charge -lt 70 ]
+if [ $battery_charge -lt 60 ]
 then
 	if [ $current_status == "Discharging" ]
 	then
 		notify-send -i /usr/share/icons/gnome/48x48/status/battery-good-charging.png "Hey!" "Charge the damn battery!"
 	fi
-elif [ $battery_charge -gt 85 ]
+elif [ $battery_charge -gt 80 ]
 then
 	if [ $current_status == "Charging" ]
 	then
